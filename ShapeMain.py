a73edf34c027ad0ed9077b35dc673e96353cc2f2
kdeleon000@citymail.cuny.edu <kdeleon000@citymail.cuny.edu>
2023-05-14 16:28:21 -0400

    update

--- ShapeMain.py
+++ ShapeMain.py
@@ -1,36 +1,25 @@
-import random, pygame
+import ShapeFunctions as sf
 
 def run_pygame():
-    screen_dimensions = initialize_screen_size()
-    screen = initialize_pygame(screen_dimensions)
-    clock = initialize_clock()
+    screen_dimensions = sf.initialize_screen_size()
+    screen = sf.initialize_pygame(screen_dimensions)
+    clock = sf.initialize_clock()
     
     game_grid = [[0]*50 for i in range(50)]
 
     running = True
 
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+        for sf.event in sf.pygame.event.get():
+            if sf.event.type == sf.pygame.QUIT:
                 running = False
                     
         screen.fill((0,0,0))
         clock.tick(25)
-        pygame.display.flip()
+        sf.pygame.display.flip()
 
-def initialize_screen_size():
-    WIDTH = 700
-    HEIGHT = 700
-    return [WIDTH,HEIGHT]
-
-def initialize_pygame(screen_dimensions):
-    pygame.init()
-    screen = pygame.display.set_mode([screen_dimensions[0],screen_dimensions[1]])
-    return screen
-
-def initialize_clock():
-    clock = pygame.time.Clock()
-    return clock
 
 run_pygame()
-pygame.quit()
+sf.pygame.quit()
+
+
